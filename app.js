fe1f86052212d402f267d3cefa815a75db2bc6b4
Raeann Lytle <raelytle@gmail.com>
2023-11-05 14:16:59 -0500

    update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const { PORT = 3001 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`App listening at port ${PORT}`);
 });
 
