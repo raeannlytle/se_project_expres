f4c37789cee480d9802f2433aedca203f9d12bed
Raeann Lytle <raelytle@gmail.com>
2023-11-05 15:17:41 -0500

    route createItem

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
+const { createItem } = require('../controllers/clothingItem');
 
 //CRUD
 
+
 //Create
+router.post('/', createItem);
+
+module.exports = router;
