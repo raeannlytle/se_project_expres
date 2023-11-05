e3e2822c0fd2eecd55d87845b2fa062c25927961
Raeann Lytle <raelytle@gmail.com>
2023-11-05 14:08:39 -0500

    update app.js

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
