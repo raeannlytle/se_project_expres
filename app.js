d14421e8fe5565a4256d4c90ae81534c4d2f0ba8
Raeann Lytle <raelytle@gmail.com>
2023-11-06 11:46:25 -0500

    fix error in run dev

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (r) => {
+  console.log('connected to DB')}, e => console.log("DB error", e));
+
+const routes = require('./routes');
+app.use(routes)
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
