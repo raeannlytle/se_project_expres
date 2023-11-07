947666365456cfa8d2f5b73c646f23f70edea3d9
Raeann Lytle <raelytle@gmail.com>
2023-11-07 08:31:00 -0500

    add console.error to error

--- utils/errors.js
+++ utils/errors.js
@@ -1,11 +1,20 @@
 const BAD_REQUEST = 400;
 
-if(err.name === 'InvalidData') return res.status(BAD_REQUEST).send({ message: "Data is invalid" });
+if(e.name === 'InvalidData') {
+  console.error('InvalidData Error:', e);
+  return res.status(BAD_REQUEST).send({ message: "Data is invalid" });
+}
 
 const NOT_FOUND = 404;
 
-if (err.name === 'NotFound') return res.status(NOT_FOUND).send({message: 'Item not found'});
+if (e.name === 'NotFound') {
+  console.error('NotFound Error:', e);
+  return res.status(NOT_FOUND).send({message: 'Item not found'});
+}
 
 const SERVER_ERROR = 500;
 
-if (err.name === 'ServerError') return res.status(SERVER_ERROR).send({message: "Server not found"});
+if (e.name === 'ServerError') {
+  console.error('ServerError Error:', e);
+  return res.status(SERVER_ERROR).send({message: "Server not found"});
+}
