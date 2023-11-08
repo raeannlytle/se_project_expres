1495e2c64f9321e6ad6bac88e61c8d74b0264cf3
Raeann Lytle <raelytle@gmail.com>
2023-11-08 13:29:01 -0500

    fix middleware issue

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (e) => {
   }
 });
 
-app.use(express.json());
-app.use(routes);
-
 app.use((req, res, next) => {
   req.user = {
     _id: '65491b3290b7482b56d92e12'
   };
   next();
 });
+
+
+app.use(express.json());
+app.use(routes);
+
 
 
 app.listen(PORT, () => {
