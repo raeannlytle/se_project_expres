277d7477485da0d2d81c542debde8300432f97f2
Raeann Lytle <raelytle@gmail.com>
2023-11-07 22:05:29 -0500

    define e, to fix error

--- utils/errors.js
+++ utils/errors.js
@@ -1,26 +1,27 @@
-const BAD_REQUEST = 400;
+const handleErrorResponse = (res, e) => {
+  const BAD_REQUEST = 400;
+  const NOT_FOUND = 404;
+  const SERVER_ERROR = 500;
 
-if(e.name === 'InvalidData') {
-  console.error('InvalidData Error:', e);
-  return res.status(BAD_REQUEST).send({ message: "Data is invalid" });
-}
+  if (e.name === 'InvalidData') {
+    console.error('InvalidData Error:');
+    return res.status(BAD_REQUEST).send({ message: 'Data is invalid' });
+  }
 
-const NOT_FOUND = 404;
+  if (e.name === 'NotFound') {
+    console.error('NotFound Error:');
+    return res.status(NOT_FOUND).send({ message: 'Item not found' });
+  }
 
-if (e.name === 'NotFound') {
-  console.error('NotFound Error:', e);
-  return res.status(NOT_FOUND).send({message: 'Item not found'});
-}
+  if (e.name === 'ServerError') {
+    console.error('ServerError Error:');
+    return res.status(SERVER_ERROR).send({ message: 'Server not found' });
+  }
 
-const SERVER_ERROR = 500;
-
-if (e.name === 'ServerError') {
-  console.error('ServerError Error:', e);
-  return res.status(SERVER_ERROR).send({message: "Server not found"});
-}
+  console.error('Other Error:', e);
+  return res.status(SERVER_ERROR).send({ message: 'Internal Server Error' });
+};
 
 module.exports = {
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR
-}
+  handleErrorResponse,
+};
