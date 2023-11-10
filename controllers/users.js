cab207bf5170a7f9b81aa3f468ff9a69bcbb82ae
Raeann Lytle <raelytle@gmail.com>
2023-11-10 08:48:28 -0600

    create login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const {BAD_REQUEST, SERVER_ERROR, NOT_FOUND} = require('../utils/errors');
+const {JWT_SECRET} = require('../utils/config');
 
 const getUser = (req, res) => {
   const {userId} = req.params;
@@ -32,28 +35,55 @@ const getUsers = (req, res) => {
 const createUser = (req, res) => {
   const {name, avatar} = req.body;
 
-  if (!name || !avatar) {
-    return res.status(BAD_REQUEST).send({message: "Name and avatar are required"})
+  if (!name || !avatar || !email || !password) {
+    return res.status(BAD_REQUEST).send({message: "All fields are required"})
   }
 
-  const newUser = new User({name, avatar});
-
-  newUser.save()
-  .then((user) => {
-    res.status(200).send({data: user});
-  })
-  .catch((e) => {
-    if (e.name === 'ValidationError') {
-      res.status(BAD_REQUEST).send({message: "Validation error"});
-    } else {
-      res.status(SERVER_ERROR).send({message: "Error from createUser"});
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(BAD_REQUEST).send({ message: "Email is already in use" });
     }
-  });
-  return null;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, avatar, email, password: hashedPassword });
+
+    const savedUser = await newUser.save();
+
+    res.status(200).send({ data: savedUser });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(BAD_REQUEST).send({ message: "Validation error" });
+    } else {
+      res.status(SERVER_ERROR).send({ message: "Error from createUser" });
+    }
+  }
+};
+
+const login = async (req, res) => {
+  const {email, password} = req.body;
+
+  try {
+    const user = await User.findUserbyCredentials(email, password);
+
+    if (!user) {
+      return res.status(UNAUTHORIZED).send({message: "Invalid email or password"});
+    }
+
+    const token = jwt.sign({_id: user._id}, JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    res.status(200).send({token})
+  } catch(e) {
+    res.status(SERVER_ERROR).send({message: "Error from login controller"});
+  }
 }
 
 module.exports = {
   getUser,
   getUsers,
-  createUser
+  createUser,
+  login
 }
