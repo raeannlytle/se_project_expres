1bc90b14054a187f47dd879343b79a43bbb5e37f
Raeann Lytle <raelytle@gmail.com>
2023-11-10 09:30:55 -0600

    create middleware auth

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../utils/config');
+const { UNAUTHORIZED } = require('../utils/errors');
+
+const authMiddleware = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization || authorization.startsWith('Bearer ')) {
+    return next();
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+
+    req.user = payload;
+
+    next();
+  } catch(e) {
+    res.status(UNAUTHORIZED).send({message: "Unauthorized"});
+  }
+};
+
+module.exports = authMiddleware;
--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,9 @@ const {getUsers, getUser, createUser} = require('../controllers/users');
 // CRUD
 
 // Create
-router.post('/', createUser);
+
 
 // Get
-router.get('/', getUsers);
-router.get('/:userId', getUser);
+
 
 module.exports = router;
