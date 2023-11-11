12ac0af1181e0530b31fd7f64b442f3a0e82fd77
Raeann Lytle <raelytle@gmail.com>
2023-11-11 07:59:40 -0600

    remove unused function

--- utils/errors.js
+++ utils/errors.js
@@ -5,41 +5,6 @@ const NOT_FOUND = 404;
 const CONFLICT = 409;
 const SERVER_ERROR = 500;
 
-const handleErrorResponse = (res, e) => {
-  if (e.name === 'InvalidData') {
-    console.error('InvalidData Error:');
-    return res.status(BAD_REQUEST).send({ message: 'Data is invalid' });
-  }
-
-  if (e.name === "Unauthorized") {
-    console.error('Unauthorized Error:');
-    return res.status(UNAUTHORIZED).send({message: "Unauthorized access"});
-  }
-
-  if (e.name === "Forbidden") {
-    console.error("Forbidden Error: ");
-    return res.status(FORBIDDEN).send({message: 'Action is forbidden'});
-  }
-
-  if (e.name === 'NotFound') {
-    console.error('NotFound Error:');
-    return res.status(NOT_FOUND).send({ message: 'Item not found' });
-  }
-
-  if (e.name === "Conflict") {
-    console.error('Conflict Error: ');
-    return res.status(CONFLICT).send({message: 'Conflict with existing resource'})
-  }
-
-  if (e.name === 'ServerError') {
-    console.error('ServerError Error:');
-    return res.status(SERVER_ERROR).send({ message: 'Server not found' });
-  }
-
-  console.error('Other Error:', e);
-  return res.status(SERVER_ERROR).send({ message: 'Internal Server Error' });
-};
-
 
 module.exports = {
   BAD_REQUEST,
@@ -47,6 +12,5 @@ module.exports = {
   NOT_FOUND,
   CONFLICT,
   SERVER_ERROR,
-  UNAUTHORIZED,
-  handleErrorResponse,
+  UNAUTHORIZED
 };
