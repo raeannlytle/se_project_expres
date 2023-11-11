aa89904571ada8e39543961af58799b5d9b91b36
Raeann Lytle <raelytle@gmail.com>
2023-11-10 23:09:51 -0600

    fix condition in  middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,8 +5,8 @@ const { UNAUTHORIZED } = require('../utils/errors');
 const authMiddleware = (req, res, next) => {
   const authorization = req.headers.authorization;
 
-  if (!authorization || authorization.startsWith('Bearer ')) {
-    return next();
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(UNAUTHORIZED).send({message: 'Unauthorized'})
   }
 
   const token = authorization.replace('Bearer ', '');
