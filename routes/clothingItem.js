8f886aea2266bd550cdd6146c6b896db5fc4af8b
Raeann Lytle <raelytle@gmail.com>
2023-11-11 15:09:55 -0600

    add middleware to routes

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authMiddleware = require('../middlewares/auth');
 
 const { createItem, getItems, deleteItem, likeItem, unlikeItem } = require('../controllers/clothingItem');
 
@@ -6,18 +7,18 @@ const { createItem, getItems, deleteItem, likeItem, unlikeItem } = require('../c
 
 
 // Create
-router.post('/', createItem);
+router.post('/', authMiddleware, createItem);
 
 // Read
 router.get('/', getItems);
 
 // Delete
-router.delete('/:itemId', deleteItem);
+router.delete('/:itemId', authMiddleware, deleteItem);
 
 // Like
-router.put('/:itemId/likes', likeItem);
+router.put('/:itemId/likes', authMiddleware, likeItem);
 
 // Unlike
-router.delete('/:itemId/likes', unlikeItem);
+router.delete('/:itemId/likes', authMiddleware, unlikeItem);
 
 module.exports = router;
