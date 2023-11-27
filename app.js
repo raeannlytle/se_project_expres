d0d822cc8d9b8381ddea7355c053efb4363d8c82
Raeann Lytle <raelytle@gmail.com>
2023-11-27 15:04:49 -0500

    update app.js

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
-const {login, createUser} = require('./controllers/users');
+const { login, createUser } = require('./controllers/users');
 
 const app = express();
 const { PORT = 3001 } = process.env;
+const DB_URL = 'mongodb://127.0.0.1:27017/wtwr_db';
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (e) => {
-  if (e) {
-    console.error('DB error', e);
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+  if (error) {
+    console.error('Failed to connect to the database:', error);
+  } else {
+    console.log('Connected to the database');
   }
 });
 
@@ -21,7 +24,15 @@ app.post('/signin', login);
 
 app.use(routes);
 
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
 
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ success: false, error: err.message });
+  }
+
+  res.status(500).json({ success: false, error: 'Internal Server Error' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
