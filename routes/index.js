c65019b576747aafa1c74f38401fd87269a99f0f
Raeann Lytle <raelytle@gmail.com>
2023-11-30 13:44:21 -0500

    update push/pull

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const clothingItem = require('./clothingItem');
-const user = require('./users');
-const authMiddleware = require('../middlewares/auth');
-const { NOT_FOUND } = require('../utils/errors');
+const router = require("express").Router();
+const clothingItem = require("./clothingItem");
+const user = require("./users");
+const authMiddleware = require("../middlewares/auth");
+const { NOT_FOUND } = require("../utils/errors");
 
-router.use('/items', clothingItem);
-router.use('/users', authMiddleware, user);
+router.use("/items", authMiddleware, clothingItem);
+router.use("/users", authMiddleware, user);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Route not found" });
