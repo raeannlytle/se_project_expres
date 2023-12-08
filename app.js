e13e68898190cc31bc092e0f5bc19b8bb34a50a1
Raeann Lytle <raelytle@gmail.com>
2023-12-08 13:54:19 -0500

    install mongoose

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes');
-const {login, createUser} = require('./controllers/users');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
+const { login, createUser } = require("./controllers/users");
+const errorHandler = require("./middlewares/error-handler");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (e) => {
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (e) => {
   if (e) {
-    console.error('DB error', e);
+    console.error("DB error", e);
   }
 });
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post("/signup", createUser);
+app.post("/signin", login);
 
 app.use(routes);
 
-
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
