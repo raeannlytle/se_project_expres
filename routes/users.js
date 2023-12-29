9f7038b706972262043c303f91834f5f99d7c98d
Raeann Lytle <raelytle@gmail.com>
2023-12-29 08:07:59 -0500

    reviewers comments

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-const router = require('express').Router();
-const authMiddleware = require('../middlewares/auth');
+const { celebrate, Joi, Segments } = require("celebrate");
+const router = require("express").Router();
+const authMiddleware = require("../middlewares/auth");
 
-const {getCurrentUser, updateUserProfile} = require('../controllers/users');
+const { getCurrentUser, updateUserProfile } = require("../controllers/users");
 
-router.get('/me', authMiddleware, getCurrentUser);
-router.patch('/me', authMiddleware, updateUserProfile);
+const updateUserProfileSchema = {
+  [Segments.BODY]: {
+    name: Joi.string(),
+    avatar: Joi.string().uri(),
+  },
+};
+
+router.get("/me", authMiddleware, getCurrentUser);
+router.patch(
+  "/me",
+  authMiddleware,
+  celebrate(updateUserProfileSchema),
+  updateUserProfile,
+);
 
 module.exports = router;
