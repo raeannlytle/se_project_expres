9ee8bce46ca6db8cd788c33592d86ff7b2dc4d8b
Raeann Lytle <raelytle@gmail.com>
2023-12-29 08:00:28 -0500

    reviewers comments

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const clothingItem = require("./clothingItem");
 const user = require("./users");
 const authMiddleware = require("../middlewares/auth");
-const { NOT_FOUND } = require("../utils/errors");
+const { NotFoundError } = require("../utils/errors/NotFoundError");
 
 router.use("/items", clothingItem);
 router.use("/users", authMiddleware, user);
 
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Route not found" });
+router.use((req, res, next) => {
+  next(new NotFoundError("Route not found"));
 });
 
 module.exports = router;
+
